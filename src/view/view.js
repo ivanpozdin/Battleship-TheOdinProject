9e99a2f94f545ec572467f767a1d700ed910195d
ivanpozdin <ivanllpozdin@gmail.com>
2023-11-23 01:17:03 +0400

    Add player names to boards

--- src/view/view.js
+++ src/view/view.js
@@ -4,11 +4,21 @@ export default function createStartView() {
   header.textContent = "Battleship";
   document.body.insertAdjacentElement("beforeend", header);
 
+  document.title = "Battleship";
+
   const boardsContainer = document.createElement("div");
 
   boardsContainer.className = "boards-container";
 
   document.body.insertAdjacentElement("beforeend", boardsContainer);
+
+  const leftPlayerName = document.createElement("div");
+  leftPlayerName.className = "left-player-name player-name";
+  leftPlayerName.textContent = "Player";
+
+  const rightPlayerName = document.createElement("div");
+  rightPlayerName.className = "right-player-name player-name";
+  rightPlayerName.textContent = "Computer";
 
   const leftBoard = document.createElement("div");
   leftBoard.className = "board";
@@ -18,6 +28,8 @@ export default function createStartView() {
   rightBoard.className = "board";
   rightBoard.id = "right-board";
 
+  boardsContainer.insertAdjacentElement("beforeend", leftPlayerName);
+  boardsContainer.insertAdjacentElement("beforeend", rightPlayerName);
   boardsContainer.insertAdjacentElement("beforeend", leftBoard);
   boardsContainer.insertAdjacentElement("beforeend", rightBoard);
 }
